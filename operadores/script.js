35899af735aa0793f3cf9eee5c9ee6d2e602dede
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-07-30 21:00:31 -0300

    strings

---
+++ operadores/script.js
@@ -0,0 +1,26 @@
+// 1- number
+console.log(2)// quando nao esta dentro de aspas numerico caso esteja la dentro de '' e uma string
+console.log(typeof 3)
+console.log(typeof 5.14)
+console.log(typeof -5)
+ 
+//operacoes artimeticas
+console.log(2 + 4)
+console.log(10-11)
+console.log(5*3)
+console.log(10/2)
+ 
+console.log(3+(5*2))
+console.log(5+(4/2)*2)
+
+//special number
+console.log(typeof Infinity) // o I DEVE SER MAIUSCULA
+console.log(typeof -Infinity)
+console.log(2 * "waw")
+console.log(typeof NaN) // ele e um number mas sem valor
+
+
+//strings podes ser em '' ou "" e `` 
+console.log("Um texto")
+console.log('Um texto')
+console.log(`Um texto`)
