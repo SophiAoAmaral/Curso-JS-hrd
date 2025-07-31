c7578531dd8020e93f044e7c6006bb6e4c03ff25
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-07-31 17:47:21 -0300

    variaveis

---
+++ estruturas/js/script.js
@@ -0,0 +1,14 @@
+// VARIAVEIS
+let nome = 'Sophia';
+console.log(nome)
+
+console.log(`Ola ${nome}`)
+
+nome = "Sophia Oliveira"
+console.log(`Ola ${nome}`)
+
+const idade = 19// const nao mudam
+
+
+
+
