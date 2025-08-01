0328369de64087d9e07dd546d4aea13838cb5ecb
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-07-31 21:46:01 -0300

    comeco math

--- estruturas/js/script.js
+++ estruturas/js/script.js
@@ -10,5 +10,27 @@ console.log(`Ola ${nome}`)
 const idade = 19// const nao mudam
 
 
+let a= 10, 
+    b=21, 
+    c=30;
+
+console.log(a, b, c)
+
+const nomeCompleto= 'Sophia Oliveira'
+const nomecompleto = 'Kayky Vinicius'
+console.log(nomeCompleto, nomecompleto)// nao da erro qpq as variaveis tem nomes diferentes pois o js identific a
 
 
+
+// prompt recebe o dado de uma var const x = prompt()
+//const age = prompt('digite sua idade')// quadro na tela
+console.log(`Sua idade e ${age}`)
+
+//o alert mostra um alert na tela
+//const z = 10
+//alert(`${z}`)
+
+//math e um objeto que possui funcoes para fins matematicos
+//max= encontra o maior valor
+// floor = arredinda para baixo o numero
+console.log(Math.max(1, 2, 3, 2, 3, 2, 5, 10))
