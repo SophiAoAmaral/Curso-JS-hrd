0fa4d5c9d860d6658ca91bd276d0a3353120e6aa
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-08-08 22:07:46 -0300

    add e removendo uma propriedade de array

--- arrayseobjetos/script.js
+++ arrayseobjetos/script.js
@@ -3,45 +3,4 @@ const lista = [1, 2, 3, 4, 5]
 
 console.log(lista)
 
-const itensc= ["Sophia", true, 2, 3, 12, []]
-
-console.log(itensc)
-
-//mais sobre arrays
-const arr= ["a", "b", "c"]
-
-console.log(arr[2])
-
-//propriedades
-const numbers = [5, 3, 4]
-
-console.log(numbers.length)
-
-const myName = "Sophia"
-console.log(myName.length);
-
-//metodos sao como funcoes acessamos com notacao de ponto e utilizamos () para invocar
-const otherNumbers = [1, 2, 3]
-
-const allNumbers = numbers.concat(otherNumbers);
-
-console.log(allNumbers)
-
-const text = "Algum texto"
-
-console.log(text.toLocaleUpperCase())
-console.log(text.indexOf("l"))
-
-//objetos
-
-const person= {
-    name: "Sophia",
-    age:19,
-    job : "Programador"
-
-}
-console.log(person.name)
-console.log(person.age)
-console.log(person.name.length)
-console.log(typeof person)
-
+c
