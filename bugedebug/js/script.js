04bf0296d682a77f6ca7ed27111faa77f1e89548
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-08-19 20:28:01 -0300

    começo seçao debug e bug

---
+++ bugedebug/js/script.js
@@ -0,0 +1,3 @@
+//scrict é um modo de desenvolvimento que deixa o JS mais rigoroso na hora de programar deve ser decaradono topo do arquivo ou de funçoes 
+//bibliotecas famosas sao todas feitas em strit
+
