df004f23a7562ee6a90a075f51522b2b5bfbdce8
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-08-19 20:30:53 -0300

    explicando como funciona strict

--- bugedebug/js/script.js
+++ bugedebug/js/script.js
@@ -1,3 +1,9 @@
 //scrict é um modo de desenvolvimento que deixa o JS mais rigoroso na hora de programar deve ser decaradono topo do arquivo ou de funçoes 
 //bibliotecas famosas sao todas feitas em strit
 
+"use strict" //vai mostrar o erro
+
+
+//opa="texto";
+const opa ="texto"
+const undefided = 10 //mostra o erro pq é do javascrit
