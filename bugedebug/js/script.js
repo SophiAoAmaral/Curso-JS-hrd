b31029efdbfb91c2453e3b485a182d3769cdec17
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-08-19 21:57:14 -0300

    tratamento de dados

--- bugedebug/js/script.js
+++ bugedebug/js/script.js
@@ -27,3 +27,36 @@ if(a>5){
 }
 
 //podemos verificar pelo console.log
+
+//debugger é uma instrução que nos permite o debug no console do navegador 
+
+let c =1
+let d= 2
+if(c==1){
+    c =d+1
+}
+
+//debugger;// se passa o mouse no console ele mostra o valor da var 
+for (let i=0; i<d;i++){
+    c= c+2
+}
+
+//tratamento de dado por funçao sempre que recebe um dado de input deve verificar e o tratamento dos dados inseridos no unput
+
+function checkNumber(n){
+
+    const result = Number(n) //converte todos os dados para number
+
+    if(Number.isNaN(result)){
+        console.log("Valor incorreto")
+        return
+    }else{
+    console.log("valor correto!")
+    return result
+    }
+}
+checkNumber(3)
+checkNumber("")
+checkNumber({})
+checkNumber(a)
+
