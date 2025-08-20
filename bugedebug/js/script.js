df8577d094e310712260ae4bad64e8765a900f8e
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-08-19 21:47:21 -0300

    console.log

--- bugedebug/js/script.js
+++ bugedebug/js/script.js
@@ -7,3 +7,23 @@
 //opa="texto";
 const opa ="texto"
 const undefided = 10 //mostra o erro pq é do javascrit
+
+
+//O METODO LOG DOS CONSOLES É MUITO UTIL
+let a = 1
+let b=2
+if(a==1){
+    a=b+2
+}
+
+console.log(a)
+
+for(let i=0; i<b;i++){
+    a = a+2
+    console.log(a)
+}
+if(a>5){
+    a=25
+}
+
+//podemos verificar pelo console.log
