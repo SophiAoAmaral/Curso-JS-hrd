1c3693e6a6542584b2ca2e9f48ed21c9d866cd9b
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-08-25 18:25:52 -0300

    SetTimeout

---
+++ AYSNC/js/script.js
@@ -0,0 +1,11 @@
+//setTimeout
+
+console.log("Ainda nao executou")
+
+setTimeout(function(){
+    console.log("Requisição assincrona")
+
+},2000)//em milesegundos 2000=2seg
+
+
+console.log("Ainda nao executou")
