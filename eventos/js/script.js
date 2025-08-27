64de9aa7558f1281e943169d371c2bad9b244e20
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-08-27 18:07:01 -0300

    eventos

--- eventos/js/script.js
+++ eventos/js/script.js
@@ -5,3 +5,21 @@ const btn = document.querySelector("#my-button")
 btn.addEventListener("click", function(){
     alert("clicou em mim")
 })
+
+//removendo evento
+
+const secondbtn = document.querySelector("#btn")
+
+function imprimirMensagem(){
+    console.log("teste")
+}
+
+secondbtn.addEventListener("click", imprimirMensagem);
+
+
+const thrdBttn = document.querySelector("#other-btn");
+thrdBttn.addEventListener("click", ()=>{
+    
+    console.log("Evento removido")
+    secondbtn.removeEventListener("click", imprimirMensagem)
+})
